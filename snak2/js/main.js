9eb666fc1224615ccbe4cc9337f75dea4e67e279
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-05-03 12:15:47 +0200

    [FIX] fixed error i is not defined

--- snak2/js/main.js
+++ snak2/js/main.js
@@ -2,6 +2,7 @@
 
 let parola1 = prompt('inserisci parola')
 let parola2 = prompt('inserisci parola')
+let i, d;
 
 for (let i = 0; i < parola1.length; i++) {
 }
@@ -9,7 +10,7 @@ for (let i = 0; i < parola1.length; i++) {
 for (let d = 0; d < parola2.length; d++) {
 }
 
-if(i>d){
+if(i > d){
   console.log(parola2);
   console.log(parola1);
 }else{
